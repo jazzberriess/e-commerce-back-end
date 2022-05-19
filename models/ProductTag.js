79633b70dd5f79a8cbb046313385af83ec1eb869
Jazzberries <anime.flick.chick@gmail.com>
2022-05-19 11:32:17 +1000

    Definied columns and added comments in Product Tag

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -2,11 +2,31 @@ const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection');
 
-class ProductTag extends Model {}
+class ProductTag extends Model { }
 
 ProductTag.init(
   {
-    // define columns
+    // define columns id, product id and tag id. Ensure that the product id is the foreign key to the id primary key in Product. Ensure that the tag is is the foreign key to the id primary key in Tag.
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    productId: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'Product',
+        key: 'id'
+      }
+    },
+    tagId: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'Tag',
+        key: 'id'
+      }
+    },
   },
   {
     sequelize,
