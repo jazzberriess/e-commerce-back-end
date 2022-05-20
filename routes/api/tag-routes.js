1e6ee828e1907b856f0b45029db880a6a1a5700d
Jazzberries <anime.flick.chick@gmail.com>
2022-05-20 15:58:36 +1000

    Added create Tags post route

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -41,8 +41,16 @@ router.get('/:id', async (req, res) => {
 
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new tag
+  try {
+    //create a new tag based on the data entered into the request body.
+    const createTag = await Tag.create(req.body);
+
+    res.status(200).json(createTag);
+  } catch (error) {
+    res.status(400).json(error)
+  }
 });
 
 router.put('/:id', (req, res) => {
